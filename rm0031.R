7a6e240d15851b769eb303bf65cc8d23bd7481d4
tk_park <hanapae@gmail.com>
2021-12-20 13:39:21 +0900

    rmd-->R

--- rm0031.R
+++ rm0031.R
@@ -1,35 +1,37 @@
----
-  title: 'test1'
-output:
-  html_document: default
-pdf_document: default
-word_document: default
----
-  
-  ```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+# ---
+#   title: 'test1'
+# output:
+#   html_document: default
+# pdf_document: default
+# word_document: default
+# ---
+#   
+#   ```{r setup, include=FALSE}
+# knitr::opts_chunk$set(echo = TRUE)
+# ```
+# 
+# 
+# ### R-program / Spanning Test of Bitcoin 
+# ---
+#   variable | Ticker | explanation 
+# :-------- | :----- | :-------------
+#   yi0       |BTC     |Bitcoin   
+# yi1       |TSLA    |Tesla, Inc   
+# yi2       |SOXX    |iShares Trust - iShares Semiconductor ETF   
+# yi3       |VOO     |Vanguard S&P 500 ETF   
+# xi1       |DBC     |Invesco DB Commodity Index Tracking Fund     
+# xi2       |GLD     |SPDR Gold Shares     
+# xi3       |QQQ     |Invesco QQQ Trust  
+# xi4       |SPY     |SPDR S&P 500 ETF Trust     
+# xi5       |TLT     |iShares 20+ Year Treasury Bond ETF     
+# 
+# This is the price from yahoo.finance. For more details on price see <http://finance.yahoo>.  
+# Risk free T-bill : Market Yield on U.S. Treasury Securities at 1-Month Constant Maturity (DGS1MO)
+# Start date : mm/dd/yyyy ???
+#   
+#   ```{r ,echo=FALSE,message=FALSE}
 
 
-### R-program / Spanning Test of Bitcoin 
----
-  variable | Ticker | explanation 
-:-------- | :----- | :-------------
-  yi0       |BTC     |Bitcoin   
-yi1       |TSLA    |Tesla, Inc   
-yi2       |SOXX    |iShares Trust - iShares Semiconductor ETF   
-yi3       |VOO     |Vanguard S&P 500 ETF   
-xi1       |DBC     |Invesco DB Commodity Index Tracking Fund     
-xi2       |GLD     |SPDR Gold Shares     
-xi3       |QQQ     |Invesco QQQ Trust  
-xi4       |SPY     |SPDR S&P 500 ETF Trust     
-xi5       |TLT     |iShares 20+ Year Treasury Bond ETF     
-
-This is the price from yahoo.finance. For more details on price see <http://finance.yahoo>.  
-Risk free T-bill : Market Yield on U.S. Treasury Securities at 1-Month Constant Maturity (DGS1MO)
-Start date : mm/dd/yyyy ???
-  
-  ```{r ,echo=FALSE,message=FALSE}
 # install.packages('rmarkdown')
 # install.packages('tinytex')
 # tinytex::install_tinytex() # install TinyTeX
@@ -37,11 +39,7 @@ Start date : mm/dd/yyyy ???
 library(dplyr)
 library(car)
 library(lmtest)
-```
 
-
-
-```{r }
 
 
 csvfile <-'~/SRP001/Srp002/data004.csv'
@@ -62,11 +60,7 @@ xi3 <- df$QQQ
 xi4 <- df$SPY
 xi5 <- df$TLT
 
-```
 
-
-
-```{r }
 ############################################################
 # test Bitcoin-usd
 ols0 <- lm(yi0 ~ xi1 + xi2 + xi3 + xi4 + xi5 , data = df)
@@ -74,6 +68,7 @@ summary(ols0)
 bptest(ols0) #BP-Test 
 
 lhs <- rbind(c(1,0,0,0,0,0),c(0,1,1,1,1,1))
+head(lhs)
 lht(ols0,lhs,c(0,1))
 
 ############################################################
@@ -97,7 +92,4 @@ ols3 <- lm(yi3 ~ xi1 + xi2 + xi3 + xi4 + xi5 , data = df)
 summary(ols3)
 lhs <- rbind(c(1,0,0,0,0,0),c(0,1,1,1,1,1))
 lht(ols3,lhs,c(0,1))
-
-
-```
 
