0b1b39b8da950a1bb48a13356905367564a28eca
tk_park <hanapae@gmail.com>
2021-12-29 18:44:31 +0900

    1 for repeat + j

--- data-filter1.R
+++ data-filter1.R
@@ -109,9 +109,11 @@ write.csv(data01, file = "data1225.csv", row.names = F)
 # 2 step test
 #######################################################
 # test Bitcoin-usd
+
+# test Bitcoin-usd
 data01 <- read.csv("data1225.csv")
 # Bitcoin ###########################################
-# Bitcoin ###########################################
+
 btc.ols <- lm(BTCUSDR ~ VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
                 GSGR + VNQR + VNQIR
               , data = data01)
@@ -127,7 +129,7 @@ lhs <- c(1,0,0,0,0,0,0,0,0,0,0)
 btc.step1 <- lht(btc.ols,lhs,c(0))
 btc.step1
 ##### step- test 2 : 
-##### unresticted : regression with alpho = 0 
+##### unresticted : regression with alpha = 0 
 btc.ols2 <- lm(BTCUSDR ~ 0 +VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
                  GSGR + VNQR + VNQIR
                , data = data01)
@@ -139,17 +141,213 @@ btc.step2
 
 summ(btc.ols,  digit = 3)  # unrestricted regression
 summ(btc.ols2,  digit = 3)  # unrestricted regression on condition alpha = 0
-
-kable(list( btc.ols$coefficients,btc.ols2$coefficients )) 
-btc.ols$coefficients
-sum(btc.ols$coefficients)
+kable(list( coef(btc.ols),coef(btc.ols2) )) # coefficient 
 
 kable(btc.hk)             # HK test, alpha = 0 and beta = 1
 kable(btc.step1)          # step test 1, alpha = 0
-kable(btc.step2)        # step test 2, beta = 1 on condition alpha = 0
+kable( btc.step2 )        # step test 2, beta = 1 on condition alpha = 0
 
 plot_summs(btc.ols, scale = TRUE, plot.distributions = TRUE, inner_ci_level = .9)
+plot_summs(btc.ols, btc.ols2, scale = TRUE)
 
+export_summs(btc.ols, btc.ols2, scale = TRUE)
+#########################################################
+#########################################################
+#########################################################
+
+########################################################
+########################################################
+# Bitcoin ###########################################
+
+data01 <- read.csv("data1225.csv")
+data01 <- data01 %>%  
+               select(Date , contains("TNX"), ends_with("R")) 
+data02 <- data01 %>% filter(Date < '2020-01-01') %>% 
+               select(Date , contains("TNX"), ends_with("R")) 
+data03 <- data01 %>% filter(Date >= '2020-01-01') %>%
+               select(Date , contains("TNX"), ends_with("R"))
+data_j <- list(data01,data02,data03)
+
+formula <- list(); model <- list() ; HK_test <- list()
+step1_test <- list() ; step2_test <- list()
+formula_con <- list() ; model_con <- list() # formaula condition on alpha = 0
+yi <- list("BTCUSDR","ETHUSDR","BNBUSDR")
+
+
+for (j in 1:3) {
+    for (i in 1:3) {
+    
+    formula[[i]] = paste0(yi[i] , " ~ ", "VTIR + VVR + VBR + IDHQR + VWOR + BNDR",
+                 "+ TIPR + GSGR + VNQR + VNQIR")
+                
+    model[[i]] <- lm(formula[[i]], data = data_j[[j]])
+    
+    ##### HK test
+    lhs <- rbind(c(1,0,0,0,0,0,0,0,0,0,0),c(0,1,1,1,1,1,1,1,1,1,1))
+    HK_test[[i]] <- lht(model[[i]],lhs,c(0,1))
+   
+    ##### step-1 test  : alpha = 0
+    lhs <- c(1,0,0,0,0,0,0,0,0,0,0)
+    step1_test[[i]] <- lht(model[[i]],lhs,c(0))
+    
+    ##### step- test 2 : 
+    ##### unresrticted model condition on alpha =0  
+    formula_con[[i]] = paste0(yi[i] , " ~ ","0 + ", "VTIR + VVR + VBR + IDHQR ",
+                          "+ VWOR + BNDR + TIPR + GSGR + VNQR + VNQIR")
+        model_con[[i]] <- lm(formula_con[[i]], data = data01)
+        ##### step- test 2 : beta=1 condition alpha = 0
+        lhs <- rbind(c(1,1,1,1,1,1,1,1,1,1))
+        step2_test[[i]] <- lht(model_con[[i]],lhs,c(1))
+    }
+}
+
+
+
+c1 <- c("BTCUSDR","ETHUSDR","BNBUSDR")
+
+names(model) <- c1
+names(model_con) <- c1
+names(HK_test) <- c1
+names(step1_test) <- c1
+names(step2_test) <- c1
+
+model
+summary(model$BTCUSDR)
+summary(model$ETHUSDR)
+summary(model$BNBUSDR)
+
+HK_test
+step1_test
+step2_test
+
+kable(HK_test)
+kable(step1_test)
+kable(step2_test)
+
+
+
+
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+#########################################################
+
+
+
+
+
+
+
+
+
+
+
+HK_test$BTCUSDR
+#  summary(HK_test$BTCUSDR)
+
+summary(model[[3]])
+summary(model[3])
+summary(model$BTCUSDR)
+print(model$BTCUSDR)
+print(model$BTCUSDR$terms)
+
+model
+class(model)
+class(model[1])
+class(model[[1]])
+
+summary(model)
+summary(model[1])
+summary(model[[1]])
+
+
+
+for ( i in 1:10) {
+     y = 10 + 5*x
+     print(y)
+     summary(model[[i]])
+   }
+
+attach(ggplot2::diamonds)
+strCols = names(ggplot2::diamonds)
+
+formula <- list(); model <- list()
+for (i in 1:1) {
+  formula[[i]] = paste0(strCols[7], " ~ ", strCols[7+i])
+  model[[i]] = glm(formula[[i]]) 
+  
+  #then you can plot or do anything else with the result ...
+  png(filename = sprintf("diamonds_price=glm(%s).png", strCols[7+i]))
+  par(mfrow = c(2, 2))      
+  plot(model[[i]])
+  dev.off()
+}
+
+
+ yi <- list("BTCUSDR","ETHUSDR","BNBUSDR")
+ formula = paste0(yi[2], " ~ ", "VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
+                 GSGR + VNQR + VNQIR")
+ yi_lm <- lm(formula, data = data01)
+ summary(yi_lm)
+
+
+btc.ols <- lm(BTCUSDR ~ VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
+                GSGR + VNQR + VNQIR
+              , data = data01)
+summary(btc.ols)
+
+btc.ols <- lm(ETHUSDR ~ VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
+                GSGR + VNQR + VNQIR
+              , data = data01)
+summary(btc.ols)
+# intercept = 0
+btc.ols2 <- lm(BTCUSDR ~ 0 +VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
+                 GSGR + VNQR + VNQIR
+               , data = data01)
+########################################################
+#
+#  function
+#
+########################################################
+wald_regress <- function(x) {
+  print(x)
+
+summary(btc.ols)
+##### HK test
+lhs <- rbind(c(1,0,0,0,0,0,0,0,0,0,0),c(0,1,1,1,1,1,1,1,1,1,1))
+btc.hk <- lht(btc.ols,lhs,c(0,1))
+btc.hk
+##### step- test 1 : alpha = 0
+lhs <- c(1,0,0,0,0,0,0,0,0,0,0)
+btc.step1 <- lht(btc.ols,lhs,c(0))
+btc.step1
+##### step- test 2 : 
+##### unresticted : regression with alpha = 0 
+summary(btc.ols2)
+##### resticted model : beta = 0 condition on alpha = 0
+lhs <- rbind(c(1,1,1,1,1,1,1,1,1,1))
+btc.step2 <- lht(btc.ols2,lhs,c(1))
+btc.step2           
+
+summ(btc.ols,  digit = 3)  # unrestricted regression
+summ(btc.ols2,  digit = 3)  # unrestricted regression on condition alpha = 0
+kable(list( coef(btc.ols),coef(btc.ols2) )) # coefficient 
+
+kable(btc.hk)             # HK test, alpha = 0 and beta = 1
+kable(btc.step1)          # step test 1, alpha = 0
+kable( btc.step2 )        # step test 2, beta = 1 on condition alpha = 0
+
+plot_summs(btc.ols, scale = TRUE, plot.distributions = TRUE, inner_ci_level = .9)
+plot_summs(btc.ols, btc.ols2, scale = TRUE)
+
+export_summs(btc.ols, btc.ols2, scale = TRUE)
 # eterrium ###########################################
 # eterrium ###########################################
 olseth <- lm(ETHUSDR ~ VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
@@ -163,7 +361,8 @@ lht(olseth,lhs,c(0,1))
 lhs <- c(1,0,0,0,0,0,0,0,0,0,0)
 lht(olseth,lhs,c(0))
 
-
+}
+wald_regress('test regression')
 # bnb coin ############################################
 olsbnb <- lm(BNBUSDR ~ VTIR + VVR + VBR + IDHQR + VWOR + BNDR + TIPR + 
              GSGR + VNQR + VNQIR
