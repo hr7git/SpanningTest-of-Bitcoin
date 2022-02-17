912e81dad13e2b1c62bb3afe45cff1585fcbb8b7
tk_park <hanapae@gmail.com>
2022-02-17 17:50:33 +0900

    1

--- fun_data.R
+++ fun_data.R
@@ -93,8 +93,8 @@ load("data_getsymbols.RData")
     #  after covid19
     load("rets2.Rdata")
     
-    # data <- rets2              # all period
-    # data <- rets2["/2019"]  # before covid19
+    data <- rets2              # all period
+    data <- rets2["/2019"]  # before covid19
     data <- rets2["2020/"]      # after covid19
     ####
     testset <- c('BTC', 'ETH', 'SNP', 'TLT' ,'GLD')
@@ -118,7 +118,8 @@ load("data_getsymbols.RData")
     t_plot <- tailoredFrontierPlot2(object = longFrontier,  twoAssets = TRUE,
                                     title = FALSE, 
                         risk = "Cov", sharpeRatio = FALSE, xlim = c(0,6))
-    t_plot
+    # t_plot
+    # t_line <- tangencyLines(object = longFrontier)
     # Frontier of benchmark
     longFrontier <- eff_Frontier2
     # twoAssetsLines(object = longFrontier,col = c("Red"))
@@ -128,15 +129,16 @@ load("data_getsymbols.RData")
     #              risk = "Cov", xlim = c(0,6), )
     #
     # b_plot <-frontierPlot(object = longFrontier, add = TRUE, title = FALSE, 
-    b_plot <-frontierPlot(object = longFrontier, add = TRUE, title = FALSE, 
+    b_plot <-frontierPlot2(object = longFrontier, add = TRUE, title = FALSE, 
                           col = c("blue","blue"), risk = "Cov", xlim = c(0,6), )
     b_line <- tangencyLines(object = longFrontier, col = "blue",
                   risk = "Cov", xlim = c(0,6), )
-    b_plot
-    b_line
+    # b_plot
+    # b_line
     #
-    test_port <- eff_Frontier@portfolio@portfolio
-    test_plot2 <- t_plot[["object"]]@portfolio
+    df1 <- data.frame(t_line)
+    df2 <- data.frame(b_line)
+    df3 <- rbind(df1,df2)
           
 ### correlation graph
 library(corrplot)
@@ -867,7 +869,7 @@ tailoredFrontierPlot2 <-
     else {
       Ylim = ylim
     }
-    frontierPlot(object, labels = FALSE, return = return, risk = risk, 
+    frontierPlot2(object, labels = FALSE, return = return, risk = risk, 
                  auto = FALSE, xlim = Xlim, ylim = Ylim, title = title, 
                  col = c("red", "red"),  # change color of frontier
                  pch = 19, ...)
@@ -879,11 +881,13 @@ tailoredFrontierPlot2 <-
     constraints <- getConstraints(object)
     mvPortfolio <- minvariancePortfolio(data, spec, constraints)
     minvariancePoints(object, return = return, risk = risk, auto = FALSE,
-                      pch = 19, col = "red")
+                      pch = 19, col = "black")
     tangencyPoints(object, return = return, risk = risk, auto = FALSE,
-                   pch = 19, col = "red")
-    tangencyLines(object, return = return, risk = risk, auto = FALSE, 
-                  col = "red")
+                   pch = 19, col = "black")
+    # tangencyLines(object, return = return, risk = risk, auto = FALSE, 
+    #               col = "red")
+    t_line <- tangencyLines(object, return = return, risk = risk, auto = FALSE, 
+                  col = "red")    # t_line add
     # xy <- equalWeightsPoints(object, return = return, risk = risk, 
     #                          auto = FALSE, pch = 15, col = "grey")
     # text(xy[, 1] + diff(xlim)/20, xy[, 2] + diff(ylim)/20, "Efficient-Frontier Line", 
@@ -903,127 +907,51 @@ tailoredFrontierPlot2 <-
     #   sharpeRatioLines(object, return = return, risk = risk, 
     #                    auto = FALSE, col = "orange", lwd = 2)
     # }
-    mtext("KDI School", adj = 0, side = 4, cex = 0.7, col = "darkgrey")
-    invisible(list(object = object, xlim = Xlim, ylim = Ylim))
+    # mtext("KDI School", adj = 0, side = 4, cex = 0.7, col = "darkgrey")
+    # invisible(list(object = object, xlim = Xlim, ylim = Ylim))
   }
 
 #############################################################################
-# function : frontierPlot2
-frontierPlot
-function (object, frontier = c("both", "lower", "upper"), 
-          col = c("black", "grey"), add = FALSE, labels = TRUE, 
-          return = c("mean", "mu"), risk = c("Cov", 
-                                             "Sigma", "CVaR", "VaR"), auto = TRUE, 
-          title = TRUE, ...) 
-{
-  stopifnot(length(col) == 2)
-  frontier <- match.arg(frontier)
-  fullFrontier = frontierPoints(object, frontier = "both", 
-                                return = return, risk = risk, auto = auto)
-  upperFrontier <- frontierPoints(object, frontier = "upper", 
+frontierPlot2 <-
+  function (object, frontier = c("both", "lower", "upper"), 
+            col = c("black", "grey"), add = FALSE, labels = TRUE, 
+            return = c("mean", "mu"), risk = c("Cov", 
+                                               "Sigma", "CVaR", "VaR"), auto = TRUE, 
+            title = TRUE, ...) 
+  {
+    stopifnot(length(col) == 2)
+    frontier <- match.arg(frontier)
+    fullFrontier = frontierPoints(object, frontier = "both", 
                                   return = return, risk = risk, auto = auto)
-  lowerFrontier <- frontierPoints(object, frontier = "lower", 
-                                  return = return, risk = risk, auto = auto)
-  Arg <- match.call(expand.dots = TRUE)
-  m <- match(c("xlim", "ylim"), names(Arg), Arg)
-  xArg <- as.character(Arg[c(1, m)])[2]
-  yArg <- as.character(Arg[c(1, m)])[3]
-  if (xArg == "NULL" & yArg == "NULL") {
-    yLim <- range(fullFrontier[, 2])
-    xRange <- range(fullFrontier[, 1])
-    xDiff <- diff(xRange)
-    xLim <- c(xRange[1] - 2.5 * xDiff/10, xRange[2] + xDiff/10)
-    if (!add) {
-      if (frontier == "upper" | frontier == "both") {
-        plot(upperFrontier, col = col[1], xlim = xLim, 
-             ylim = yLim, ann = FALSE, ...)
-      }
-      else {
-        if (frontier == "both") {
-          points(fullFrontier, col = col[2], xlim = xLim, 
-                 ylim = yLim, ...)
-        }
-        if (frontier == "lower") {
-          plot(lowerFrontier, col = col[2], xlim = xLim, 
+    upperFrontier <- frontierPoints(object, frontier = "upper", 
+                                    return = return, risk = risk, auto = auto)
+    lowerFrontier <- frontierPoints(object, frontier = "lower", 
+                                    return = return, risk = risk, auto = auto)
+    Arg <- match.call(expand.dots = TRUE)
+    m <- match(c("xlim", "ylim"), names(Arg), Arg)
+    xArg <- as.character(Arg[c(1, m)])[2]
+    yArg <- as.character(Arg[c(1, m)])[3]
+    if (xArg == "NULL" & yArg == "NULL") {
+      yLim <- range(fullFrontier[, 2])
+      xRange <- range(fullFrontier[, 1])
+      xDiff <- diff(xRange)
+      xLim <- c(xRange[1] - 2.5 * xDiff/10, xRange[2] + xDiff/10)
+      if (!add) {
+        if (frontier == "upper" | frontier == "both") {
+          plot(upperFrontier, col = col[1], xlim = xLim, 
                ylim = yLim, ann = FALSE, ...)
         }
-      }
-    }
-    if (frontier == "upper" | frontier == "both") {
-      points(upperFrontier, col = col[1], ...)
-    }
-    if (frontier == "lower" | frontier == "both") {
-      points(lowerFrontier, col = col[2], ...)
-    }
-  }
-  else if (xArg != "NULL" & yArg == "NULL") {
-    yLim = range(fullFrontier[, 2])
-    if (!add) {
-      if (frontier == "upper" | frontier == "both") {
-        plot(upperFrontier, col = col[1], ylim = yLim, 
-             ann = FALSE, ...)
-      }
-      else {
-        if (frontier == "both") {
-          points(fullFrontier, col = col[2], ylim = yLim, 
-                 ...)
-        }
-        if (frontier == "lower") {
-          plot(fullFrontier, col = col[2], ylim = yLim, 
-               ann = FALSE, ...)
+        else {
+          if (frontier == "both") {
+            points(fullFrontier, col = col[2], xlim = xLim, 
+                   ylim = yLim, ...)
+          }
+          if (frontier == "lower") {
+            plot(lowerFrontier, col = col[2], xlim = xLim, 
+                 ylim = yLim, ann = FALSE, ...)
+          }
         }
       }
-    }
-    if (frontier == "upper" | frontier == "both") {
-      points(upperFrontier, col = col[1], ...)
-    }
-    if (frontier == "lower" | frontier == "both") {
-      points(lowerFrontier, col = col[2], ...)
-    }
-  }
-  else if (xArg == "NULL" & yArg != "NULL") {
-    xRange = range(fullFrontier[, 1])
-    xDiff = diff(xRange)
-    xLim = c(xRange[1] - 2.5 * xDiff/10, xRange[2] + xDiff/10)
-    if (!add) {
-      if (frontier == "upper" | frontier == "both") {
-        plot(upperFrontier, col = col[1], xlim = xLim, 
-             ann = FALSE, ...)
-      }
-      else {
-        if (frontier == "both") {
-          points(fullFrontier, col = col[2], xlim = xLim, 
-                 ...)
-        }
-        if (frontier == "lower") {
-          plot(lowerFrontier, col = col[2], xlim = xLim, 
-               ann = FALSE, ...)
-        }
-      }
-    }
-    if (frontier == "upper" | frontier == "both") {
-      points(upperFrontier, col = col[1], ...)
-    }
-    if (frontier == "lower" | frontier == "both") {
-      points(lowerFrontier, col = col[2], ...)
-    }
-  }
-  else if (xArg != "NULL" & yArg != "NULL") {
-    if (!add) {
-      if (frontier == "upper" | frontier == "both") {
-        plot(fullFrontier, type = "n", ann = FALSE, 
-             ...)
-        points(upperFrontier, col = col[1], ...)
-      }
-      if (frontier == "both") {
-        points(lowerFrontier, col = col[2], ...)
-      }
-      if (frontier == "lower") {
-        plot(lowerFrontier, col = col[2], ann = FALSE, 
-             ...)
-      }
-    }
-    else {
       if (frontier == "upper" | frontier == "both") {
         points(upperFrontier, col = col[1], ...)
       }
@@ -1031,14 +959,88 @@ function (object, frontier = c("both", "lower", "upper"),
         points(lowerFrontier, col = col[2], ...)
       }
     }
+    else if (xArg != "NULL" & yArg == "NULL") {
+      yLim = range(fullFrontier[, 2])
+      if (!add) {
+        if (frontier == "upper" | frontier == "both") {
+          plot(upperFrontier, col = col[1], ylim = yLim, 
+               ann = FALSE, ...)
+        }
+        else {
+          if (frontier == "both") {
+            points(fullFrontier, col = col[2], ylim = yLim, 
+                   ...)
+          }
+          if (frontier == "lower") {
+            plot(fullFrontier, col = col[2], ylim = yLim, 
+                 ann = FALSE, ...)
+          }
+        }
+      }
+      if (frontier == "upper" | frontier == "both") {
+        points(upperFrontier, col = col[1], ...)
+      }
+      if (frontier == "lower" | frontier == "both") {
+        points(lowerFrontier, col = col[2], ...)
+      }
+    }
+    else if (xArg == "NULL" & yArg != "NULL") {
+      xRange = range(fullFrontier[, 1])
+      xDiff = diff(xRange)
+      xLim = c(xRange[1] - 2.5 * xDiff/10, xRange[2] + xDiff/10)
+      if (!add) {
+        if (frontier == "upper" | frontier == "both") {
+          plot(upperFrontier, col = col[1], xlim = xLim, 
+               ann = FALSE, ...)
+        }
+        else {
+          if (frontier == "both") {
+            points(fullFrontier, col = col[2], xlim = xLim, 
+                   ...)
+          }
+          if (frontier == "lower") {
+            plot(lowerFrontier, col = col[2], xlim = xLim, 
+                 ann = FALSE, ...)
+          }
+        }
+      }
+      if (frontier == "upper" | frontier == "both") {
+        points(upperFrontier, col = col[1], ...)
+      }
+      if (frontier == "lower" | frontier == "both") {
+        points(lowerFrontier, col = col[2], ...)
+      }
+    }
+    else if (xArg != "NULL" & yArg != "NULL") {
+      if (!add) {
+        if (frontier == "upper" | frontier == "both") {
+          plot(fullFrontier, type = "n", ann = FALSE, 
+               ...)
+          points(upperFrontier, col = col[1], ...)
+        }
+        if (frontier == "both") {
+          points(lowerFrontier, col = col[2], ...)
+        }
+        if (frontier == "lower") {
+          plot(lowerFrontier, col = col[2], ann = FALSE, 
+               ...)
+        }
+      }
+      else {
+        if (frontier == "upper" | frontier == "both") {
+          points(upperFrontier, col = col[1], ...)
+        }
+        if (frontier == "lower" | frontier == "both") {
+          points(lowerFrontier, col = col[2], ...)
+        }
+      }
+    }
+    if (title) {
+      labs = attr(fullFrontier, "control")
+      title(main = "Efficient Frontier", xlab = paste("Target Risk[", 
+                                                      labs[1], "]", sep = ""), ylab = paste("Target Return[", 
+                                                                                            labs[2], "]", sep = ""))
+    }
+    # mtext("Rmetrics", adj = 0, side = 4, cex = 0.7, col = "darkgrey")
+    # invisible(fullFrontier)
   }
-  if (title) {
-    labs = attr(fullFrontier, "control")
-    title(main = "Efficient Frontier", xlab = paste("Target Risk[", 
-                                                    labs[1], "]", sep = ""), ylab = paste("Target Return[", 
-                                                                                          labs[2], "]", sep = ""))
-  }
-  # mtext("Rmetrics", adj = 0, side = 4, cex = 0.7, col = "darkgrey")
-  # invisible(fullFrontier)
-}
-####################################################
